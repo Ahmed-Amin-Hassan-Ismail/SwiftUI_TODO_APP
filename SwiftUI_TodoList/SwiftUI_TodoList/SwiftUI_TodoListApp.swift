b6d1abb21f094ca3eb56d86dad72097e067ff1eb
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-10-30 23:02:51 +0200

    create xCode project with adjust compatible Xcode version and min deployment target

---
+++ SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TodoListApp.swift
+//  SwiftUI_TodoList
+//
+//  Created by Ahmed Amin on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
