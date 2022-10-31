3cf489c7eb05b8b12a7fedf0b85fad416037a0ef
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-01 00:11:24 +0200

    configure the app in both dark and light mode plus adjusted the ratio related to iPad

--- SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
+++ SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
@@ -17,6 +17,7 @@ struct SwiftUI_TodoListApp: App {
             NavigationView {
                 ListView()
             }
+            .navigationViewStyle(.stack)
             .environmentObject(listViewModel)
         }
     }
